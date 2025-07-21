2e8557a8a677bb7a9f63ca33842eb7aa91db8f87
hubism2511 <unjin95@naver.com>
2025-07-21 11:10:22 +0900

    #4 feat:shadcn ui 적용

--- src/app/layouts/header/header.tsx
+++ src/app/layouts/header/header.tsx
@@ -1,9 +1,10 @@
-import { DropDown, Search, Button } from "@/shared";
+import { DropDown, Search } from "@/shared";
+import HeaderLoginButton from "./header-login-button";
 
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-between  bg-gray-1000 text-white w-full h-full h-19 px-5">
+      <div className="flex justify-between  bg-gray-900 text-white w-full h-19 px-5">
         <div className="flex justify-start w-full h-full">
           <div className=" w-17.5 h-full">
             <div className="flex items-center text-white text-2xl font-bold font-['Inter'] w-full h-full">
@@ -21,7 +22,7 @@ export default function Header() {
               className="w-50"
             />
             <span className="w-0 h-10 mx-5 outline-1 outline-zinc-800"></span>
-            <Button text="로그인" className="w-20 h-10" />
+            <HeaderLoginButton children="로그인" />
           </div>
         </div>
       </div>
